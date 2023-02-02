2be406f78bf02f1544b7ee9a94ed825c7a0a6f32
sSsiriusSs <m.fatih.abd@gmail.com>
2023-02-02 20:58:30 +0300

    created contect structures, added buttons and consts, next html+css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount]=useState(0)
+  const [userIncreaseDecreaseValue,setUserIncreaseDecreaseValue]=useState(1)
+
+  const incrementsAndDecrements = [-100, -50, -25, -20, -10, -5, -3, -2, -1, 1, 2, 3, 5, 10, 20, 25, 50, 100]
+
+  const handleAutoCount = () => {
+    setInterval(()=>{
+      if(count === setCounterEndPoint){clearInterval(count)}
+      else {
+      setCount(prev=>prev+Number(userIncreaseDecreaseValue))}
+    },1000)
+  };
+  const handleStopCount = () => {
+    clearInterval(count);
+    setCount(0)
+  };
+
+
+  const [counterEndPoint,setCounterEndPoint]= useState(120)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='title'>
+        <h1>Counter Strike 1.3</h1>
+        <h1 className='digitalcounter'>{count}</h1>
+      </div>
+
+      <div className='countbasic'>
+        <button onClick={()=>setCount(count-Number(userIncreaseDecreaseValue))}>Decrease</button>
+        <button onClick={()=>setCount(0)}>Reset</button>
+        <button onClick={()=>setCount(count+Number(userIncreaseDecreaseValue))}>Increase</button><br /><br />
+      </div>
+      
+      <input placeholder='Type Your Increase/Decrease Value' onChange={(e)=>setUserIncreaseDecreaseValue(e.target.value)} /><br /><br />
+
+      <div className='defultIncDecValues'>
+        {incrementsAndDecrements.map((value,index)=>{
+          //The map function returns a value and  an index then we can assing them as key property and value variable
+          return <button key={index} onClick={()=>setCount(count + value)}>{value}</button> 
+        })} <br /><br />
+      </div>
+
+      <div className='endPoint'>
+        <input placeholder='Where should the counter end?' onChange={(event)=>setCounterEndPoint(event.target.value)} /> <br />
+        <label>Counter stops at {counterEndPoint} as default</label> 
+        <br /><br />
+      </div>
+        
+      
+      <div className='autoCount'>
+        <button onClick={handleAutoCount}>Start Auto Count</button>
+        <button onClick={handleStopCount}>Stop Auto Count</button>
+      </div>
+      
     </div>
   );
 }
